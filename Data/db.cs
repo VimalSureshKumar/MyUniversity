acccc40959f2b3b5af107907a5d40b7b79e2bfa0
ac116329 <ac116329@8CG8163L1Y>
2023-05-15 15:04:57 +1200

    Add project files.

---
+++ Data/db.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyUniversity.Models;
+
+namespace MyUniversityContext
+{
+    public class db : DbContext
+    {
+        public db (DbContextOptions<db> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyUniversity.Models.Student> Student { get; set; } = default!;
+
+        public DbSet<MyUniversity.Models.Enrollment> Enrollment { get; set; } = default!;
+
+        public DbSet<MyUniversity.Models.Course> Course { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyUniversityContext;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<db>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("db") ?? throw new InvalidOperationException("Connection string 'db' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
