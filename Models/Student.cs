3145c460bc3e57166c41aa67461fe72364cdbe37
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2023-05-17 12:35:12 +1200

    ..

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,20 +1,26 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyUniversity.Models
 {
     public class Student
     {
+        [NotMapped]
+        public string FullName
+        {
+            get { return FirstName + "" + LastName; }
+        }
         public int StudentID { get; set; }
-        [Display(Name = "Student ID")]
-        public string LastName { get; set; }
         [Display(Name = "Last Name")]
-        public string FirstName { get; set; }
+        public string LastName { get; set; }
         [Display(Name = "First Name")]
-        public DateTime EnrollmentDate { get; set; }
+        public string FirstName { get; set; }
         [Display(Name = "Enrollment Date")]
-        public DateTime DOB { get; set; }
+        public DateTime EnrollmentDate { get; set; }
         [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+        [DataType(DataType.Text)]
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
